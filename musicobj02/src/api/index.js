e1b149bc95be310b557ebb9d72812ffed31b3a7f
wangzhen <wangzhendashuaib@gmail.com>
2023-05-25 17:29:26 +0800

    updata5.25

---
+++ musicobj02/src/api/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost:3000";
+
+//封装获取轮播图的api  调用例子：/banner ,/banner?type=2
+export  function getBanner(type=0){
+    return axios.get(`${baseUrl}/banner?type=${type}`);
+
+}
+
+export default {getBanner}
